14c6914c5cdf306f98834c9bedc6c6ebf0317b2a
AMG <alimertgulec38@gmail.com>
2023-01-09 14:29:02 +0300

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Ali Mert Güleç on 9.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
